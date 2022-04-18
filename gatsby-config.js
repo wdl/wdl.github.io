603748b51f7f0bc78a1a4d4696be2a770902d1ad
Elenchus <sijongyeoil@gmail.com>
2022-04-18 17:48:41 +0900

    set google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,12 +48,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `G-QJ69E2J7RJ`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
